a248af43b99eb48459110b58776a9264ec8045e4
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-04-25 21:35:23 -0500

    refactor facebook.js: decouple DOM binding and fb api calls; update devise initializer to request scope permissions; unified clientside and serverside auth flows for facebook

--- config/routes.rb
+++ config/routes.rb
@@ -11,11 +11,11 @@ Weave::Application.routes.draw do
 
   resources :referrers
 
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match 'auth/failure', to: redirect('/')
+  # match 'auth/:provider/callback', to: 'sessions#create'
+  # match 'auth/failure', to: redirect('/')
   # match 'signout', to: 'sessions#destroy', as: 'signout'
 
-  match 'af', to: redirect("/auth/facebook")
+  # match 'af', to: redirect("/auth/facebook")
   match 'welcome', to: 'referrers#welcome'
 
   root to: 'referrals#new'
