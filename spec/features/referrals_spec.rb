ed92d94ac0851496d86b11ba2b785e6e47386b5f
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-05-10 00:22:03 -0500

    Fix testing stub javascript include tag; setting referral feature spec to pending for now

--- spec/features/referrals_spec.rb
+++ spec/features/referrals_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe "zugReferrals" do
   describe "stuff" do
-    it "works", js: true do
-      binding.pry
+    pending "works", js: true do
+      click '#sign-in-facebook'
+      complete_facebook_connect_and_wait_for "Weave"
       visit  '#/stories/1/referrals/1'
-      save_and_open_page
     end
   end
 end
