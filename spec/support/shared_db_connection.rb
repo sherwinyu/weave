7a074be0a66c63ae60c51dc048585d99ffe0cebf
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-05-10 18:46:09 -0500

    add documentation to spec/support/shared_db_connection.rb

--- spec/support/shared_db_connection.rb
+++ spec/support/shared_db_connection.rb
@@ -10,3 +10,5 @@ end
 # Forces all threads to share the same connection. This works on
 # Capybara because it starts the web server in a thread.
 # ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+#
+# Commented out because of http://railscasts.com/episodes/391-testing-javascript-with-phantomjs?view=comments#comment_163505
