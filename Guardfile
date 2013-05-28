37cf03af1f5d5a465d5f0abae38008cc8b0c715b
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-05-27 22:38:02 -0500

    modifying Guardfile + spec_helper in preparation for zeus and removing spork

--- Guardfile
+++ Guardfile
@@ -1,6 +1,7 @@
 # A sample Guardfile
 # More info at https://github.com/guard/guard#readme
 
+=begin
 guard 'spork', :cucumber_env => { 'RAILS_ENV' => 'test' }, :rspec_env => { 'RAILS_ENV' => 'test' } do
   watch('config/application.rb')
   watch('config/environment.rb')
@@ -13,8 +14,10 @@ guard 'spork', :cucumber_env => { 'RAILS_ENV' => 'test' }, :rspec_env => { 'RAIL
   watch(%r{features/support/}) { :cucumber }
   watch(%r{^spec/support/(.+)\.rb$})
 end
+=end
 
-guard 'rspec', :version => 2, cli: "--drb", all_on_start: false do
+# guard 'rspec', :version => 2, cli: "--drb", all_on_start: false do
+guard 'rspec', :version => 2, cli: "--drb", zeus: true, all_on_start: false do
   watch(%r{^spec/.+_spec\.rb$})
   watch(%r{^lib/(.+)\.rb$})     { |m| "spec/lib/#{m[1]}_spec.rb" }
   watch('spec/spec_helper.rb')  { "spec" }
@@ -48,3 +51,32 @@ notification :tmux,
   line_separator: ' > ',
   color_location:  'status-left-bg'
 
+
+=begin
+guard 'spring' do
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^spec/spec_helper\.rb$})                   { |m| 'spec' }
+  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/#{m[1]}_spec.rb" }
+  watch(%r{^lib/(.+)\.rb$})                           { |m| "spec/lib/#{m[1]}_spec.rb" }
+  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  do |m|
+    %W(spec/routing/#{m[1]}_routing_spec.rb spec/#{m[2]}s/#{m[1]}_#{m[2]}_spec.rb spec/requests/#{m[1]}_spec.rb)
+  end
+end
+
+guard 'zeus', test_unit: false, bundler: false do
+  # uses the .rspec file
+  # --colour --fail-fast --format documentation --tag ~slow
+  watch(%r{^spec/.+_spec\.rb$})
+  watch(%r{^app/(.+)\.rb$})                           { |m| "spec/#{m[1]}_spec.rb" }
+  watch(%r{^app/(.+)\.haml$})                         { |m| "spec/#{m[1]}.haml_spec.rb" }
+  watch(%r{^lib/(.+)\.rb$})                           { |m| "spec/lib/#{m[1]}_spec.rb" }
+  watch(%r{^app/controllers/(.+)_(controller)\.rb$})  { |m| ["spec/routing/#{m[1]}_routing_spec.rb", "spec/#{m[2]}s/#{m[1]}_#{m[2]}_spec.rb", "spec/requests/#{m[1]}_spec.rb"] }
+
+  # TestUnit
+  # watch(%r|^test/(.*)_test\.rb$|)
+  # watch(%r|^lib/(.*)([^/]+)\.rb$|)     { |m| "test/#{m[1]}test_#{m[2]}.rb" }
+  # watch(%r|^test/test_helper\.rb$|)    { "test" }
+  # watch(%r|^app/controllers/(.*)\.rb$|) { |m| "test/functional/#{m[1]}_test.rb" }
+  # watch(%r|^app/models/(.*)\.rb$|)      { |m| "test/unit/#{m[1]}_test.rb" }
+end
+=end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,82 @@
 require 'rubygems'
-require 'spork'
+# require 'spork'
 require 'capybara/rspec'
 require 'capybara/poltergeist'
 #uncomment the following line to use spork with the debugger
 #require 'spork/ext/ruby-debug'
 
+
+###!!
+###!!            No spork
+###!!           ==========
+
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+# from https://gist.github.com/rsanheim/1054078
+
+RSpec.configure do |config|
+  # == Mock Framework
+  #
+  # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
+  #
+  # config.mock_with :mocha
+  # config.mock_with :flexmock
+  # config.mock_with :rr
+  config.mock_with :rspec
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  # If you're not using ActiveRecord, or you'd prefer not to run each of your
+  # examples within a transaction, remove the following line or assign false
+  # instead of true.
+  config.use_transactional_fixtures = true
+
+  # Allows FactoryGirl build, create, etc methods to be called from a global
+  # scope (without prefacing with 'FactoryGirl.x')
+  config.include FactoryGirl::Syntax::Methods
+
+  # Include devise helpers and controller macros (https://github.com/plataformatec/devise/wiki/How-To%3a-Controllers-and-Views-tests-with-Rails-3-%28and-rspec%29)
+  config.include Devise::TestHelpers, :type => :controller
+  # config.extend ControllerMacros, :type => :controller
+
+end
+# Use poltergeist
+Capybara.register_driver :poltergeist_debug do |app|
+  Capybara::Poltergeist::Driver.new(app, inspector: true)
+end
+
+Capybara.javascript_driver = :poltergeist_debug
+
+
+# This code will be run each time you run your specs.
+FactoryGirl.reload
+
+# https://gist.github.com/mrinterweb/850225
+# Devise caches user class so we need to reload it
+load File.expand_path(File.dirname(__FILE__) + '/../app/models/user.rb')
+
+# from http://stackoverflow.com/questions/14061325/prepare-called-on-a-closed-database-rails-rspec
+# ActiveRecord::Base.establish_connection
+ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
+
+
+
+
+
+
+
+
+
+=begin
 Spork.prefork do
   # Loading more in this block will cause your tests to run faster. However,
   # if you change any configuration or code from libraries loaded here, you'll
@@ -72,6 +144,7 @@ Spork.each_run do
   # ActiveRecord::Base.establish_connection
   ActiveRecord::Base.shared_connection = ActiveRecord::Base.connection
 end
+=end
 
 # --- Instructions ---
 # Sort the contents of this file into a Spork.prefork and a Spork.each_run
