de0b79f6776c8f78f9bfc5a479e5a6f52d2cd35b
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-06-06 16:20:51 -0500

    configure email to default to canonical_email in UserSerializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,4 +1,7 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :canonical_name, :canonical_email, :email_provided
   has_many :authorizations, embed: :objects
+  def email
+    object.email || object.canonical_email
+  end
 end
