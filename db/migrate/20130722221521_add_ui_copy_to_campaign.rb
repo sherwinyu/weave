c19dc3fde47d876349c5d006b622c1a2889f2cb7
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-07-25 14:21:24 -0500

    update clietns and copy migrations

--- db/migrate/20130722221521_add_ui_copy_to_campaign.rb
+++ db/migrate/20130722221521_add_ui_copy_to_campaign.rb
@@ -8,8 +8,8 @@ class AddUiCopyToCampaign < ActiveRecord::Migration
     client = Client.find_by_name "New Living"
     if client
       logger.info "Adding UI Copy to campaign: found a client named New Living: #{client}"
-      client.update_attribute referral_message: "I just shopped at New Living, a mission-driven Certified Benefit Corporation that has made a commitment to measure success on a social, environmental and economic level. I know you care a lot about where you shop, so I thought I'd let you know about New Living.",
-      client.update_attribute intro_message: "Tell your friends about New Living's socially responsible products. Get a $50 Whole Foods Gift Card!",
+      client.update_attribute referral_message: "I just shopped at New Living, a mission-driven Certified Benefit Corporation that has made a commitment to measure success on a social, environmental and economic level. I know you care a lot about where you shop, so I thought I'd let you know about New Living."
+      client.update_attribute intro_message: "Tell your friends about New Living's socially responsible products. Get a $50 Whole Foods Gift Card!"
     end
   end
 end
