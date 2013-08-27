4ef906935a2996abffdc5f16ec2fdce3e72273e3
Sherwin Yu <sherwin.x.yu@gmail.com>
2013-08-27 10:44:49 -0500

    commenting out template name check in referral mailer spec

--- spec/mailers/referral_mailer_spec.rb
+++ spec/mailers/referral_mailer_spec.rb
@@ -10,6 +10,8 @@ describe ReferralMailer do
       r.customizations << customization
       r
     end
+    before :each do
+    end
     context "method: sender_to_recipient_referral_newliving" do
       before :each do
         ReferralMailer.should_receive(:sender_to_recipient_referral_newliving).and_call_original
@@ -24,11 +26,24 @@ describe ReferralMailer do
     context "method: sender_to_recipient" do
       before :each do
         ReferralMailer.should_receive(:sender_to_recipient).and_call_original
-        ReferralMailer.deliver referral, options
+        puts "delivered"
+        deliver!
       end
+      let(:deliver!) { ReferralMailer.deliver referral, options }
       let(:options){ {method: :sender_to_recipient} }
+
       it "calls sender_to_recipient_referral_newliving with the referral" do
         ReferralMailer.should have_received(:sender_to_recipient).with referral, an_instance_of(Hash)
+      end
+      pending "check template_name" do
+        let(:deliver!) { nil }
+        it "renders 'sender_to_recipient'" do
+          ReferralMailer.any_instance.stub(:render)
+          binding.pry
+          expect { ReferralMailer.deliver referral, options }.to raise_error /Bad Request/
+          ReferralMailer.any_instance.should have_received(:render) #.with('sender_to_recipient_new_living')
+        end
+
       end
     end
   end
